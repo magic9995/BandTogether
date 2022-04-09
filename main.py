39ec0cfcf0f1daba3505efec78a6872a3ce7afc7
magic9995 <wiigopher@gmail.com>
2022-04-08 22:31:13 -0400

    created intial file, to be fleshed out later

---
+++ main.py
@@ -0,0 +1,38 @@
+import pandas as pd
+import spotipy
+
+class User:
+
+    def __init__(self):
+
+        pass
+
+    def getSongData(self):
+
+        pass
+
+    ## Insert functions here
+
+class App:
+
+    def __init__(self):
+
+        pass
+
+    def getUserData(self):
+
+        pass
+
+    def Login(self):
+
+        pass
+
+    def compare(self, User):
+
+        pass
+
+    def enterInDB(self, User):
+
+        pass
+
+    

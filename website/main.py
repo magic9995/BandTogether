be6b189016cd90458b7b90ec6c55c7c6771a4e68
magic9995 <wiigopher@gmail.com>
2022-04-09 20:29:20 -0400

    prepare for main.py merge

--- website/main.py
+++ website/main.py
@@ -1,5 +1,6 @@
-from wsgiref.util import request_uri
+#from wsgiref.util import request_uri
 from flask import Flask, request, render_template
+from app import User, App
 
 app = Flask(__name__)
 app.config['HOSTNAME'] = "Rohan"
@@ -11,11 +12,25 @@ def index():
 @app.route('/loading', methods=['POST'])
 def dashboard():
 
-    if request.method == 'POST':
-       username = request.form["email"]
-       password = request.form["password"]
+   if request.method == 'POST':
+      username = request.form["email"]
+      password = request.form["password"]
+      
+   user = User()
+   appl = App()
+   info = appl.getTopAlbumsArtists(user)
+   albums = info[0]
+   artists = info[1]
+   images = []
+   for link in albums.keys():
+      images.append(albums[link])
+   
+   topArtists = []
+   for artist in artists.keys():
+      topArtists.append(artist)
 
-    return render_template("loading.html", username=username, password=password)
+   return render_template("loading.html", username=username, password=password, images=images,
+                           artists = topArtists, lenArtists = len(artists))
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', threaded=True)
+   app.run(debug=True, host='0.0.0.0', threaded=True)

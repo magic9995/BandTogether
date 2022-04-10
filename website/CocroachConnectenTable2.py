e51c413ad7abef499ac6c84ce2680c88c27245cc
platypus-perry <103296480+platypus-perry@users.noreply.github.com>
2022-04-10 01:12:49 -0400

    Add files via upload

--- website/CocroachConnectenTable2.py
+++ website/CocroachConnectenTable2.py
@@ -62,11 +62,7 @@ def returnSpotifyDataOfUsername(conn,username):
         )
         result = cur.fetchone()
     conn.commit()
-    return {"username": result[0], "liveness": result[1], "valence": result[2], "danceability": result[3], 
-    "loudness": result[4], "mode": result[5], 
-    "acousticness": result[6], "instrumentalness": result[7], 
-    "tempo": result[8], "energy": result[9],
-    "longitude": result[10], "latitude": result[11] }
+    return(result)
 
 def returnDataOfTableInList(conn):
     listOutput = []
@@ -105,6 +101,11 @@ def containsUser(conn,username):
         
 
 
-def getConn():
+def main():
     conn = psycopg2.connect("postgresql://vaidya45:xTFH37o0EEDY3gOd-UyZrw@free-tier11.gcp-us-east1.cockroachlabs.cloud:26257/defaultdb?sslmode=verify-full&options=--cluster%3Dblast-horgi-470")
-    return conn
+    create_table(conn)
+    print(returnUserLocation(conn,"username2"))
+    conn.close()
+
+if __name__ == "__main__":
+    main() 
